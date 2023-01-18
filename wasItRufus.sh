c5e8e5cc6cefa94dfadb676eae1d101ac266335d
gsg27 <gsg27@sfu.ca>
2023-01-17 21:01:47 -0800

    output name of current branch using revparse

--- wasItRufus.sh
+++ wasItRufus.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
 #Was it Rufus? script for Pronot.ai
+#reference used: https://www.taniarascia.com/how-to-create-and-use-bash-scripts/
 
 who="World"
 echo "Hello, $who!"
+
+#Get current branch status using rev-parse
+current_branch=$(git rev-parse --abbrev-ref HEAD) 
+echo "$current_branch"
