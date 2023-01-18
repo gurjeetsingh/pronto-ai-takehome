d14f674af07908986c07d5aab4ace83aca2d7593
gsg27 <gsg27@sfu.ca>
2023-01-17 23:55:04 -0800

    cleaned up and tested

--- wasItRufus.sh
+++ wasItRufus.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
-#Was it Rufus? script for Pronot.ai
-#reference used: https://www.taniarascia.com/how-to-create-and-use-bash-scripts/
+#Was it Rufus? script for Pronot.ai take home
+#Name: Gurjeet Singh Gill
+#Education: Computer Science Graduate
+#Email: gurjeetsg1@gmail.com
+#LinkedIn: https://www.linkedin.com/in/gurjeetsinghgill/
+#references used: https://www.taniarascia.com/how-to-create-and-use-bash-scripts/
 
-who="World"
-echo "Hello, $who!"
 
 #Get current branch status using rev-parse
-
+#references used: https://git-scm.com/docs/git-rev-parse
 activeBranch=$(git rev-parse --abbrev-ref HEAD) 
 echo "active branch: $activeBranch"
 
 #Wheter repository files have been moified
-
 branchStatus=$(git status --porcelain)
 if [ -z "$branchStatus" ]
 then
@@ -22,7 +23,8 @@ else
 fi
 
 #Whether the current head commit was authored in the last week
-#reference: https://manpages.ubuntu.com/manpages/xenial/man1/date.1.html
+#reference used: https://manpages.ubuntu.com/manpages/xenial/man1/date.1.html
+#reference used: https://git-scm.com/docs/git-log
 #seconds in 1 week = 604800
 
 timeOfCommit=$(git log -1 --date=unix --format=%ad)
@@ -38,7 +40,6 @@ fi
 
 
 #Whether the current head commit was authored by Rufus
-
 lastAuthor=$(git log -1 --format=%an)
 echo "$lastAuthor"
 if [ "$lastAuthor" == "Rufus" ]
