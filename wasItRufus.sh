f0541cde14d0070385b384cb87fad59d5590e0f7
gsg27 <gsg27@sfu.ca>
2023-01-17 22:35:32 -0800

    figured out getting time difference

--- wasItRufus.sh
+++ wasItRufus.sh
@@ -8,15 +8,26 @@ echo "Hello, $who!"
 
 #Get current branch status using rev-parse
 
-active_branch=$(git rev-parse --abbrev-ref HEAD) 
-echo "active branch: $active_branch"
+activeBranch=$(git rev-parse --abbrev-ref HEAD) 
+echo "active branch: $activeBranch"
 
 #Wheter repository files have been moified
 
-branch_status=$(git status --porcelain)
-if [ -z "$branch_status" ]
+branchStatus=$(git status --porcelain)
+if [ -z "$branchStatus" ]
 then
 	echo "local changes: True"
 else
 	echo "local changes: False"
 fi
+
+#Whether the current head commit was authored in the last week
+#reference: https://manpages.ubuntu.com/manpages/xenial/man1/date.1.html
+#seconds in 1 week = 
+
+timeOfCommit=$(git log -1 --format=%cd)
+currentTime=$(date +%s)
+timeSince=$(currentTime - (date -d timeOfCommit +%s)) #how to get difference
+echo "$timeOfCommit"
+echo "$currentTime"
+echo "$timeSince"
