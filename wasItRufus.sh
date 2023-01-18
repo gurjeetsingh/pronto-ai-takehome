273a4a2ceeb1e864f032bcae7b5e20564bb3ac6c
gsg27 <gsg27@sfu.ca>
2023-01-17 21:17:00 -0800

    active branch and local changes working

--- wasItRufus.sh
+++ wasItRufus.sh
@@ -7,5 +7,16 @@ who="World"
 echo "Hello, $who!"
 
 #Get current branch status using rev-parse
-current_branch=$(git rev-parse --abbrev-ref HEAD) 
-echo "active branch: $current_branch"
+
+active_branch=$(git rev-parse --abbrev-ref HEAD) 
+echo "active branch: $active_branch"
+
+#Wheter repository files have been moified
+
+branch_status=$(git status --porcelain)
+if [ -z "$branch_status" ]
+then
+	echo "local changes: True"
+else
+	echo "local changes: False"
+fi
